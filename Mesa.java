d2dc4a035f8fe43c781a99369e13b0e2e7ede728
Laosline <lhgois.campos@hotmail.com>
2018-07-26 01:45:31 -0300

    Lucas

--- Mesa.java
+++ Mesa.java
@@ -3,30 +3,45 @@ public class Mesa {
 	private Pote pote = new Pote();
 	private Mao jogador1 = new Mao();
 	private Mao jogador2 = new Mao();
-	private int rodada = 1;
+	private int rodada;
 
-	public void comecarJogo(){
-		//checagem
+	public int comecarJogo(){
+		// checagem
 		// setar o jogador da rodada (1 ou 2)
+		return rodada;
 	}
 	
-	public void jogada(){
-		mostrarMao();	
+	public void jogada(int rodada){
+		int pedraEscolhida;
+
+		this.rodada = rodada;
+		mostrarMao();
+		//pedraEscolhida = scanear pedra escolhida
+		if(rodada%2 != 0){
+			inserirPedra(jogador1.vetorPedra[pedraEscolhida]);
+			jogador1.retiraPedra(pedraEscolhida);
+		} else if(rodada%2 == 0){
+			inserirPedra(jogador1.vetorPedra[pedraEscolhida]);
+			jogador1.retiraPedra(pedraEscolhida);
+		}	
 	}
 	
+	public void inserirPedra(Pedra pedraNova){
+		if(principal.getValor1 == pedraNova.getValor1 || principal.getValor2 == pedraNova.getValor1)
+			principal.setValor1 = pedraNova.getValor2;
+		if(principal.getValor2 == pedraNova.getValor1 || principal.getValor2 == pedraNova.getValor2)
+			principal.setValor2 = pedraNova.getValor1;
+		// caso contrario, jogada invalida
+	}
+
 	public Pedra getPrincipal (){
 		return principal;
 	}
 
-	public void clear(){
-		for(int i=0;i<10;i++)
-			System.out.println("\n");
-	}
-
 	public void mostrarMao(){
-		if(rodada == 1)
+		if(rodada != 0)
 			abrirJanela.abrir(jogador1.imprimirMao());
-		if(rodada == 2)
+		if(rodada == 0)
 			abrirJanela.abrir(jogador2.imprimirMao());
 	}
 }
---
+++ Principal.java
@@ -0,0 +1,13 @@
+public class Principal{
+	public static void main(String[] args) {
+		Mesa mesa = new Mesa();
+		int rodada = mesa.comecarJogo();
+	
+    		while(true){
+			mesa.jogada(rodada); 
+			//rodada impar => vez do jogador 1
+			//rodada par => vez do jogador 2
+			rodada++;
+		}
+	}
+}
