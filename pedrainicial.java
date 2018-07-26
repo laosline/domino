5ce9d276b4c8f5888b2fdb045a96c6288b32f18a
mrizzi1104 <32569695+mrizzi1104@users.noreply.github.com>
2018-07-26 15:48:54 -0300

    Update pedrainicial.java

--- pedrainicial.java
+++ pedrainicial.java
@@ -10,7 +10,8 @@ public pedra igual (mao mao, pedra referencia, int jogador) // funcao que faz a 
 					{
 						if(mao.PedraMao[i].valor1 > referencia.valor1)  // checa se o valor da pedra igual e melhor
 							{
-								referencia = setvalor ( mao.PedraMao[i].valor1 , mao.PedraMao[i].valor2); // atualiza valor da pedra referencia
+								referencia = setValor1 ( mao.PedraMao[i].valor1); 
+								referencia = setValor2 ( mao.PedraMao[i].valor2); // atualiza valor da pedra referencia
 							}
 					}
 			}
@@ -29,7 +30,8 @@ public pedra maior ( mao mao, pedra referencia, int jogador)
 				Pedra ( mao.PedraMao[i]); // pega valor da pedra
 				if( (mao.PedraMao[i].valor1 + mao.PedraMao[i].valor2) > maiorPedra)  // verifica se a peca e maior
 					{
-						referencia = setValor ( mao.PedraMao[i].valor1 , mao.PedraMao[i].valor2);	
+						referencia = setValor1 ( mao.PedraMao[i].valor1); 
+						referencia = setValor2 ( mao.PedraMao[i].valor2);	
 					}
 			}
 
@@ -44,11 +46,12 @@ public pedra pedraInicial( mao mao1, mao mao2)
 	{
 		Pedra pedraIgual;	//variavel que seleciona pedra
 		pedraIgual = new pedra();	// instancia objeto do tipo pedra
-		pedraIgual = setValor(-1,-1);   // inicia valor de pedra 
+		pedraIgual = setValor1(-1);
+		pedraIgual = setValor2(-1);   // inicia valor de pedra 
 		int jogador = 0;		// variavel que vai dizer qual jogador comeca
 		pedraIgual = igual( mao1, pedraIgual, 1);	// chama funcao igual que verifica se existe peca com valores iguais da mao 1
  		pedraIgual = igual ( mao2, pedraIgual, 2);
-		if( pedraIgual.valor1 == -1)
+		if( pedraIgual.valor1 == -1)	//se nao achar peca igual, vai pra peca maior
 			{
 				pedraIgual = maior (mao1, 1);
 				pedraigual = maior (mao2, 2);
