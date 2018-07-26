f747fc37282a9ef5baf616cb9b0ff52ca59d37e8
laosline <32200215+laosline@users.noreply.github.com>
2018-07-26 14:40:15 -0300

    Update Mao.java

--- Mao.java
+++ Mao.java
@@ -7,12 +7,16 @@ public class Mao extends Conjunto{
 
     public void compraInicial(){
         for (int i = 0; i < 7; i++){
-            compraPedra();
+            compraPedraInicial();
         }
     }
-
+    /** private int maiorDuplo = -1
+        if (pedraComprada.getValor1() == pedraComprada.getValor2()){
+                maiorDuplo = pedraComprada.getValor1();
+        }**/
     public void compraPedra (Pedra pedraComprada){
         vetorPedras[numPedras] = pedraComprada;
+
         numPedras++;
     }
 }
