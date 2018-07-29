4f402229a6e6c20805c66aae3ccce587cbfad2cd
Felipe Andrade <41554760+FRAndrade@users.noreply.github.com>
2018-07-28 22:19:40 -0300

    Create campeao.java

---
+++ campeao.java
@@ -0,0 +1,23 @@
+public class campeao extends Exception{
+	int jogador;
+	public campeao(int i){
+		this.jogador = i;
+	}
+	public String getMessage(){
+		return "O jogador";
+	}
+	public String getMessage1(){
+		return " ganhou o jogo";
+	}
+	public String getMessage2(){
+		return "Parabéns Campeão!!!";
+	}
+	public double getCampeao(){
+		if(jogador%2 != 0){
+			return "1"
+		}
+		if(jogador%2 == 0){
+			return "2"
+		}
+	}
+}
